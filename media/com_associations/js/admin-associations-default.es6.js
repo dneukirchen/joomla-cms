d09eaf215c03c183a46b36f534df26ca797ed3d6
David Neukirchen <david.neukirchen@gmail.com>
2018-02-12 21:41:12 +0100

    Transform admin-associations-default.js to es6.

--- media/com_associations/js/admin-associations-default.es6.js
+++ media/com_associations/js/admin-associations-default.es6.js
@@ -1,14 +1,8 @@
 /**
-* PLEASE DO NOT MODIFY THIS FILE.
-* WORK ON THE ES6 VERSION.
-* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
-**/
-
-/**
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-Joomla.submitbutton = function (pressbutton) {
+Joomla.submitbutton = (pressbutton) => {
   if (pressbutton === 'associations.purge') {
     if (window.confirm(Joomla.JText._('COM_ASSOCIATIONS_PURGE_CONFIRM_PROMPT'))) {
       Joomla.submitform(pressbutton);
